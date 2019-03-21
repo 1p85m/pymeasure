af0c6a9d7773184130f42d8b06ac60fde2dbad83
kotakayoshi <kousakaitatsu@kousakaitatsunoMacBook-Air.local>
2019-03-21 14:37:33 +0900

    for 1p85

--- device/SENA/adios.py
+++ device/SENA/adios.py
@@ -55,47 +55,47 @@ class adio(object):
         while True:
             try:
                 temp = self.com.recv()
-                if temp.find(':')==-1: continue
-                else: pass
-                #print temp
-                break
-            except communicator.CommunicatorTimeout:
+                temp1 = temp.decode()
+                if temp.find(b':')==0:
+                    break
+                else:
+                    break
+            except:
                 print('(error) att1')
-                continue
-            continue
+                break
         self.com.close()
         time.sleep(0.1)
         
-        do1 = int(temp[-16])*1
-        do2 = int(temp[-15])*2
-        do3 = int(temp[-14])*4
-        do4 = int(temp[-13])*8
-        do5 = int(temp[-11])*16
+        do1 = int(temp1[-16])*1
+        do2 = int(temp1[-15])*2
+        do3 = int(temp1[-14])*4
+        do4 = int(temp1[-13])*8
+        do5 = int(temp1[-11])*16
         att1 = do1+do2+do3+do4+do5
         return att1
 
-    
     def get_att2(self):
         self.com.open()
         self.com.send(off['level_off'])
         while True:
             try:
                 temp = self.com.recv()
-                if temp.find(':')==-1: continue
-                else: pass
-                break
-            except communicator.CommunicatorTimeout:
+                temp1 = temp.decode()
+                if temp.find(b':')==0:
+                    break
+                else:
+                    break
+            except:
                 print('(error) att2')
-                continue
-            continue
+                break
         self.com.close()
-        time.sleep(0.1)
+        time.sleep(0.1)    
 
-        do6 = int(temp[-10])*1
-        do7 = int(temp[-9])*2
-        do8 = int(temp[-8])*4
-        do9 = int(temp[-6])*8
-        do10 = int(temp[-5])*16
+        do6 = int(temp1[-10])*1
+        do7 = int(temp1[-9])*2
+        do8 = int(temp1[-8])*4
+        do9 = int(temp1[-6])*8
+        do10 = int(temp1[-5])*16
         att2 = do6+do7+do8+do9+do10
         return att2
 
